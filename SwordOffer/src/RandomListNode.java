b2aca5c2459719a5cfbe3417c8bcd04debac2dca
DreamCats <maifeng_cat@outlook.com>
2020-01-13 13:24:16 +0800

    复杂链表的复制

---
+++ SwordOffer/src/RandomListNode.java
@@ -0,0 +1,19 @@
+/**
+ * @program LeetNiu
+ * @description: 随机链表
+ * @author: mf
+ * @create: 2020/01/13 13:11
+ */
+
+public class RandomListNode {
+
+    int label;
+
+    RandomListNode next = null;
+
+    RandomListNode random = null;
+
+    public RandomListNode(int label) {
+        this.label = label;
+    }
+}
