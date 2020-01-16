ecc676963c8358587ac7d9534c8650664e872e1f
DreamCats <maifeng_cat@outlook.com>
2020-01-16 14:24:51 +0800

    TreeLinkNode

---
+++ SwordOffer/src/T57.java
@@ -0,0 +1,16 @@
+/**
+ * @program LeetNiu
+ * @description: 二叉树的下一个结点
+ * @author: mf
+ * @create: 2020/01/16 14:22
+ */
+
+/**
+ * 给定一个二叉树和其中的一个结点，请找出中序遍历顺序的下一个结点并且返回。
+ * 注意，树中的结点不仅包含左右子结点，同时包含指向父结点的指针。
+ */
+public class T57 {
+    public TreeLinkNode GetNext(TreeLinkNode pNode) {
+
+    }
+}
